8ee25ccd1603ed5ebf2b2a0ed1fd9c9a5fb32ed4
chienh77 <166679470+chienh77@users.noreply.github.com>
2024-05-02 11:13:42 +0800

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,27 @@
+1 ~ 100 中的所有奇數和
+
+【1】
+sum = 0 
+for x in range(1, 101): 
+    if x % 2 !=0:
+        sum = sum + x 
+
+print(sum) 
+
+【2】
+sum = 0 
+for x in range(1, 101): 
+    if x % 2==0:
+       continue
+    sum = sum + x 
+
+print(sum) 
+
+【3】
+sum = 0 
+for x in range(1, 101,2): 
+        sum = sum + x 
+
+print(sum) 
+
+409012569 杜芊嬅
